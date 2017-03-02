48d3aaf3f1e60f228903613a0b8aa3470f5ee5ba
Berk Arslan <berk@debian>
2017-03-02 07:06:42 -0500

    Initial commit

---
+++ src/main/java/com/blogger/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.blogger.config.audit;
---
+++ src/main/java/com/blogger/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.blogger.repository;
+
+import com.blogger.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
